b5fada068827b54cd63ccc988f32fce6dc76c0a4
Vadim Tcaregorodtcev <dev.tcaregorodtcev@gmail.com>
2020-11-30 18:22:32 +0300

    add gestures

---
+++ model.js
@@ -0,0 +1,153 @@
+let modelIsTrained = false;
+let modal;
+let cover;
+let knn;
+let featureExtractor; let classifier; let video; let loss;
+
+const onUseGesturesClick = () => {
+  if (!modelIsTrained) {
+    openTrainGesturesModal();
+  }
+}
+
+const classify = () => {
+  const logits = classifier.infer(video)
+  knn.classify(logits, gotResults);
+}
+
+const addButtonsForGestureTraining = () => {
+  const btnContainer = document.createElement('div');
+  const rightBtn = document.createElement('button')
+  const leftBtn = document.createElement('button')
+  const downBtn = document.createElement('button')
+  const upBtn = document.createElement('button')
+
+  btnContainer.classList.add('btn-container');
+  rightBtn.classList.add('btn');
+  rightBtn.innerHTML = 'Right ->'
+  leftBtn.classList.add('btn');
+  leftBtn.innerHTML = '<- Left'
+  downBtn.classList.add('btn');
+  downBtn.innerHTML = "\\/ Down"
+  upBtn.classList.add('btn');
+  upBtn.innerHTML = "^ UP"
+
+  btnContainer.append(leftBtn, upBtn, downBtn, rightBtn)
+
+  modal.appendChild(btnContainer);
+
+  return { rightBtn, leftBtn, upBtn, downBtn }
+}
+
+function gotResults(err, results) {
+  // Display any error
+  if (err) {
+    console.error(err);
+  }
+  if (results) {
+    const label = results.label;
+
+    switch (label) {
+      case 'RIGHT': snake.setDirection('right')
+        break;
+      case 'LEFT': snake.setDirection('left')
+        break;
+      case 'DOWN': snake.setDirection('down')
+        break;
+      case 'UP': snake.setDirection('up')
+        break;
+    }
+
+    classify();
+  }
+}
+
+const addExample = (label) => {
+  const logits = classifier.infer(video)
+  knn.addExample(logits, label)
+}
+
+const openTrainGesturesModal = () => {
+  openModal();
+
+  video.parent('modal')
+  video.show();
+
+  const { rightBtn, leftBtn, upBtn, downBtn } = addButtonsForGestureTraining();
+
+  rightBtn.onclick = () => addExample("RIGHT");
+  leftBtn.onclick = () => addExample("LEFT");
+  upBtn.onclick = () => addExample("UP");
+  downBtn.onclick = () => addExample("DOWN");
+
+  const btnContainer = document.createElement('div');
+  btnContainer.classList.add('btn-container');
+
+  const trainBtn = document.createElement('button')
+  trainBtn.classList.add('btn');
+  trainBtn.innerHTML = "Train and Play!"
+
+  btnContainer.append(trainBtn)
+  modal.appendChild(btnContainer);
+
+  trainBtn.onclick = () => {
+    closeModal();
+    classify();
+  }
+}
+
+const openModal = () => {
+  modal = document.createElement('div');
+  cover = document.createElement('div');
+
+  modal.classList.add('modal')
+  cover.classList.add('cover')
+
+  modal.id = 'modal';
+  cover.onclick = closeModal;
+
+  document.body.appendChild(cover);
+  document.body.appendChild(modal);
+}
+
+const closeModal = () => {
+  video.parent('body')
+  video.hide();
+
+  document.body.removeChild(modal);
+  document.body.removeChild(cover);
+}
+
+const assignHandlers = () => {
+  document.getElementById('gestures').onclick = onUseGesturesClick
+}
+
+assignHandlers();
+
+
+const modelReady = () => { }
+
+const videoReady = () => { }
+
+// ml5
+function setup() {
+  noCanvas();
+  // Create a video element
+  video = createCapture({
+    video: {
+      mandatory: {
+        maxWidth: 280,
+        maxHeight: 200
+      },
+    },
+  });
+
+  video.hide();
+  // Append it to the videoContainer DOM element
+  //video.parent('modal');
+  // Extract the already learned features from MobileNet
+  featureExtractor = ml5.featureExtractor('MobileNet', modelReady);
+  // Create a new classifier using those features and give the video we want to use
+  classifier = featureExtractor.classification(video, videoReady);
+  knn = ml5.KNNClassifier();
+}
---
+++ snake.js
@@ -0,0 +1,179 @@
+const id = 'game';
+const size = 30;
+const tick = 90;
+let gameTimeout;
+
+const toggleSnake = (body) => {
+  for (let i = 0; i < body.length; i++) {
+    const [x, y] = body[i];
+
+    document.getElementById(getCoordsSelector(x, y)).classList.toggle('snake')
+  }
+}
+
+const snake = {
+  body: [[0, 0], [0, 1], [0, 2]],
+  direction: 'right',
+
+  checkCollision() {
+    return new Set(this.body.map(coords => getCoordsSelector(coords[0], coords[1]))).size !== this.body.length
+  },
+
+  normalize([x, y], size) {
+    return [x >= 0 ? (x % size) : (size + x), y >= 0 ? (y % size) : (size + y)]
+  },
+
+  noDeth(size) {
+    this.body = this.body.map(([x, y]) => this.normalize([x, y], size))
+  },
+
+  setDirection(dir) {
+    switch (dir) {
+      case 'right': {
+        if (this.direction === 'left') return;
+        break;
+      }
+      case 'left': {
+        if (this.direction === 'right') return;
+        break;
+      }
+      case 'up': {
+        if (this.direction === 'down') return;
+        break;
+      }
+      case 'down': {
+        if (this.direction === 'up') return;
+        break;
+      }
+    }
+    this.direction = dir;
+  },
+
+  move(size, replace, isCandy, candyFound, gameOver) {
+    replace(this.body)
+
+    const head = this.body.slice(-1)[0]
+    let newHead;
+
+    switch (this.direction) {
+      case 'right': {
+        newHead = [head[0], head[1] + 1]
+        break;
+      }
+      case 'left': {
+        newHead = [head[0], head[1] - 1]
+        break;
+      }
+      case 'up': {
+        newHead = [head[0] - 1, head[1]]
+        break;
+      }
+      case 'down': {
+        newHead = [head[0] + 1, head[1]]
+        break;
+      }
+    }
+
+    if (!isCandy(this.normalize(newHead, size))) {
+      this.body.shift();
+    } else {
+      candyFound(newHead);
+    }
+
+    this.body.push(newHead);
+
+    this.noDeth(size);
+
+    if (this.checkCollision()) gameOver()
+
+    replace(this.body)
+  }
+}
+
+const getCoordsSelector = (x, y) => `x:${x}_y:${y}`;
+
+const getCell = (x, y) => {
+  const div = document.createElement('div')
+  div.classList.add('cell')
+  div.id = getCoordsSelector(x, y)
+
+  return div.outerHTML
+};
+
+const cells = (() => {
+  let cells = []
+
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      if (!cells[i]) cells[i] = [];
+
+      cells[i][j] = getCell(i, j)
+    }
+  }
+
+  return cells;
+})()
+
+const fillGame = () => {
+  const game = document.getElementById('game');
+
+  for (let i = 0; i < size; i++) {
+    const row = document.createElement('div')
+
+    row.classList.add('row')
+    row.innerHTML = cells[i].join('');
+
+    game.appendChild(row)
+  }
+}
+
+const listenClicks = () => {
+  document.addEventListener('keydown', (e) => {
+    const callback = {
+      "ArrowLeft": () => snake.setDirection('left'),
+      "ArrowRight": () => snake.setDirection('right'),
+      "ArrowUp": () => snake.setDirection('up'),
+      "ArrowDown": () => snake.setDirection('down'),
+    }[e.key]
+    callback?.()
+  })
+}
+
+const putCandy = (size) => {
+  const x = Math.floor(Math.random() * Math.floor(size));
+  const y = Math.floor(Math.random() * Math.floor(size));
+
+  document.getElementById(getCoordsSelector(x, y)).classList.add('candy')
+}
+
+const isCandy = ([x, y]) => {
+  return document.getElementById(getCoordsSelector(x, y)).classList.contains('candy')
+}
+
+const candyFound = ([x, y]) => {
+  document.getElementById(getCoordsSelector(x, y)).classList.remove('candy');
+
+  putCandy(size);
+}
+
+const gameOver = () => {
+  clearTimeout(gameTimeout)
+}
+
+const idle = () => {
+  gameTimeout = setTimeout(idle, tick)
+
+  snake.move(size, toggleSnake, isCandy, candyFound, gameOver);
+}
+
+const init = () => {
+  fillGame();
+  toggleSnake(snake.body);
+
+  putCandy(size);
+
+  listenClicks();
+  setTimeout(idle, tick);
+}
+
+init();
